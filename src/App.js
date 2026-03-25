eaf81ebdc5a11e1fb53de7a17052840019cb2c22
yitbarek <yitbarekwelde658@gmail.com>
2026-03-25 08:50:08 -0300

    handleclick btn added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+import "./styles.css";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -11,6 +15,7 @@ export default function App() {
       <Form />
       <PackingList />
       <Stats />
+      <FlashCards />
     </div>
   );
 }
@@ -20,14 +25,26 @@ function Logo() {
 }
 
 function Form() {
-function handleSubmit(e) {
-  e.preventDefault();
-}
+  const [description, setDescription] = useState("Test");
+  const [quantity, setQuantity] = useState(7);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // 👇will make the state back to it's initial state
+    setDescription("");
+    setQuantity(1);
+  }
 
   return (
-    <form className="add-form" on-submit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -35,9 +52,14 @@ function handleSubmit(e) {
         ))}
       </select>
 
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
-    </fo>
+    </form>
   );
 }
 
@@ -68,5 +90,62 @@ function Stats() {
     <footer className="stats">
       <em>👜 You have X items on your list, and you already packed X (X%)</em>
     </footer>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  const [selectedId, setselectedId] = useState(null);
+  function handleClick(id) {
+    setselectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
   );
 }
